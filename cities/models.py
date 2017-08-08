1dee0340ce52cd83f445d49394ad259c726d1fc2
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-08-08 08:23:47 +0300

    changed __str__ to City model

--- cities/models.py
+++ cities/models.py
@@ -226,7 +226,7 @@ class City(BaseCity):
 
     def __str__(self):
         if self.alt_names.filter(language_code=get_language()).count() > 1:
-            return ', '.join([i.name for i in self.alt_names.objects.filter(language_code=get_language()).all()])
+            return ', '.join([i.name for i in self.alt_names.filter(language_code=get_language()).all()])
         else:
             return self.alt_names.filter(language_code=get_language()).first().name
 
